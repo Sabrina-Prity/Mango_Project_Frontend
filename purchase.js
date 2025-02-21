d63d7ef77f84f074066d49c2fa98ecc7fd8e0308
Sabrina-Prity <sabrinaprity898@gmail.com>
2025-02-21 23:58:11 +0600

    update

--- purchase.js
+++ purchase.js
@@ -21,7 +21,6 @@ const buyNow = (mangoId, price, maxQuantity) => {
     const orderData = {
         quantity: quantity,
         buying_status: "Pending",
-        payment_status: "Pending",
         user : user_id,
         product: mangoId,
        
